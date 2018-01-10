c95826a0366d5738924ab6a1846ed3d2a8740d08
andy91to <32710942+andy91to@users.noreply.github.com>
2018-01-10 19:07:17 +0100

    Aggiunto variabili
    Iniziato la stesura del codice, ho inserito le variabili.

---
+++ src/Distributore.java
@@ -0,0 +1,15 @@
+
+public class Distributore {
+	private static int coca=10;
+	private static int fanta=10;
+	private static int acqua=0;
+	private static int[] lattine=new int[] {coca,fanta,acqua};
+	public static double totSoldiMacchinetta;
+	static int numeroTipiBibite=3;
+	static String[] tipoBibita=new String[] {"Coca","Fanta","Acqua"};
+	int indiceBibita,i;
+	public double resto;
+	public static double totMonetine;
+	
+	double[] prezzi=new double[] {0.75,0.75,0.3};
+}
