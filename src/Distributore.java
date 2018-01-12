0246da66b579ad5f4fd2cfb0f15e4047103f9d59
andy91to <32710942+andy91to@users.noreply.github.com>
2018-01-12 22:37:31 +0100

    Completato anche opzione 4
    Sembra fuznionare tutto...
    Possibile aggiunta di ulteriori funzioni

--- src/Distributore.java
+++ src/Distributore.java
@@ -1,112 +1,145 @@
+import java.util.Scanner;
 
 public class Distributore {
-	private static int coca=10;
-	private static int fanta=10;
-	private static int acqua=0;
-	private static int[] lattine=new int[] {coca,fanta,acqua};
-	public static double totSoldiMacchinetta;
-	static int numeroTipiBibite=3;
 
-//dichiarazione variabili (nome bibita,numero lattine contenute nel distributore e prezzo di ciascuna) 
+	// dichiarazione variabili (nome bibita,numero lattine contenute nel
+	// distributore e prezzo di ciascuna)
 
-	private String[] nomeBibite= {"Coca","Fanta","Acqua"};
-	private static int[] numLattine= {1,0,0};
-	private double[] prezziLattine= {0.75,0.50,0.30};
-	private String passw="admin";
-	
-	private static double denaroTotale=0;
-	private static double totMonetine=0;
+	private String[] nomeBibite = { "Coca", "Fanta", "Acqua" };
+	private static int[] numLattine = { 10, 7, 9 };
+	private double[] prezziLattine = { 0.75, 0.50, 0.30 };
+	private String passw = "admin";
+
+	private static double denaroTotale = 0;
+	private static double totMonetine = 0;
 	int indice;
-	boolean flag=false;
+	boolean flag = false;
 
 	public void contaDenaro(double monetina) {
-		totMonetine+=monetina;
+		totMonetine += monetina;
 	}
-	
+
 	private void contaGuadagnoTot(double nuovoGuadagno) {
-		denaroTotale+=nuovoGuadagno;
+		denaroTotale += nuovoGuadagno;
 	}
-	
-//GETTERS E SETTERS
-	
+
+	// GETTERS E SETTERS
+
 	public double getTotMonetine() {
 		return totMonetine;
 	}
+
 	public static double getDenaroTotale() {
 		return denaroTotale;
 	}
-	
+
 	public void mostraBevande() {
+		
+		System.out.println("Denaro a disposizione: " + totMonetine + "€");
 		System.out.println("--------------------");
 		System.out.println("Bevande disponibili: ");
 		System.out.println("--------------------");
-		System.out.println("Denaro a disposizione: "+totMonetine+"ï¿½");
 		for (int i = 0; i < nomeBibite.length; i++) {
-			if(numLattine[i]>0) {
-				System.out.println("-"+(i+1)+" "+nomeBibite[i]+" "+"prezzo "+prezziLattine[i]+"ï¿½");
-			}
-			else
-				System.out.println(nomeBibite[i]+" non disponibile");
+			if (numLattine[i] > 0) {
+				System.out.println("-" + (i + 1) + " " + nomeBibite[i] + " " + "prezzo " + prezziLattine[i] + "€");
+			} else
+				System.out.println(nomeBibite[i] + " non disponibile");
 		}
 	}
-	
+
 	public void erogaBibita(int indice) {
-		int j=indice-1;
-		if (numLattine[j]==0) {
-			System.out.println("Prodotto non disponibile,");
-			System.out.println("ripetere operazione!");
+		
+		int j = indice - 1;
+		if (numLattine[j] == 0) {
+			System.out.println("\n");
+			System.out.println("Prodotto non disponibile, ripetere operazione!");
 			ritornaSoldi();
-			
-		}
-		else if(prezziLattine[j]>totMonetine) {
+		} 
+		else if (prezziLattine[j] > totMonetine) {
+			System.out.println("\n");
 			System.out.println("Denaro insufficiente");
 			ritornaSoldi();
+		} 
+		else {
+			numLattine[j] = numLattine[j] - 1;
+			System.out.println(nomeBibite[j] + " erogata");
+			denaroTotale += prezziLattine[j];
+			System.out.println("Denaro incassato dalla macchinetta: " + denaroTotale);
+			daiResto(j);
+		}
+	}
+
+	public void ritornaSoldi() {
+		
+		System.out.println("--[Ritirare i " + totMonetine + "€ da te inseriti]--");
+		totMonetine = 0;
+	}
+
+	public void daiResto(int indice) {
+		
+		System.out.println("\n--[Ritira il resto di " + (totMonetine - prezziLattine[indice])+"]--");
+		totMonetine = 0;
+	}
+
+	public void caricaMacchinetta(String nomeB, int n) {
+		int indiceL=trovaIndice(nomeB);
+		if (n > 0) {
+			if (indiceL!=(-1)) {
+					numLattine[indiceL] += n;
+					System.out.println("\n--["+nomeBibite[indiceL] + " aggiunta correttamente!]--\n");
+					System.out.println("--------------------");
+					System.out.println("Lattine disponibili"); 
+					System.out.println("--------------------");
+					stampaLattine();
+					System.out.println("\n");
+			}
+			else {
+					System.out.println("!!!!Nome bibita inesistente\n");
+				 }
+		} else
+			System.out.println("!!!!Hai inserito un numero negativo");
+	}
+
+	public boolean verificaPassword(String password) {
+		
+		boolean passwIdentica = false;
+		if (password.equals("admin"))
+			passwIdentica = true;
+		else
+			System.out.println("\n!!!!Password sbagliata!\n");
+		return passwIdentica;
+	}
+	
+	public void avviaCarico(boolean passwordIdentica) {
+		
+		Scanner in= new Scanner(System.in);
+		String tipoLattina;
+		int numLat;
+		if(passwordIdentica) {
+			System.out.println("Password esatta procedi al carico\n");
+			System.out.println("--[Inserisci nome bibita da ricaricare]--");
+			tipoLattina=in.next();
+			System.out.println("--[Inserisci numero di lattine ricaricate]--");
+			numLat=in.nextInt();
+			caricaMacchinetta(tipoLattina, numLat);
+		}
+	}
+	private void stampaLattine() {
+		
+		for (int i = 0; i < nomeBibite.length; i++) {
+			System.out.println(nomeBibite[i] +" "+ numLattine[i]);
 			
 		}
-		else {
-			numLattine[j]=numLattine[j]-1;
-			System.out.println(nomeBibite[j]+" erogata");
-			denaroTotale+=prezziLattine[j];
-			System.out.println("Denaro incassato dalla macchinetta: "+denaroTotale);
-			daiResto(j);
-			System.out.println(numLattine[0]+" "+numLattine[1]+" "+numLattine[2]);
-		}
 	}
 	
-	public void ritornaSoldi() {
-		System.out.println("Ritirare i "+totMonetine+"ï¿½ da te inseriti");
-		totMonetine=0;
-	}
-	
-	public void daiResto(int indice) {
-		System.out.println("Ritira il resto di "+(totMonetine-prezziLattine[indice]));
-		totMonetine=0;
-	}
-	public void login(String password, String nomeB, int n) {
-		if(password.equals(passw)) {
-			caricaMacchinetta(nomeB, n);
-			System.out.println(numLattine[0]+" "+numLattine[1]+" "+numLattine[2]);
-		}
-		else {
-			System.out.println("Password non valida");
-		}
-	}
-	public void caricaMacchinetta(String nomeB,int n) {
-		if(n>0) {
-			for(int i=0;i<nomeBibite.length;i++) {
-				if (nomeBibite[i].equalsIgnoreCase(nomeB)) {
-					numLattine[i]+=n;
-					System.out.println(nomeB+" aggiunta correttamente!");
-					flag=true;
-				}
-				if(flag=false) {
-					System.out.println("Nome bibita insesistente");
-				}
+	private int trovaIndice(String nomeB) {
+		int ind=-1;
+		for (int i = 0; i < nomeBibite.length; i++) {
+			if(nomeBibite[i].equalsIgnoreCase(nomeB)) {
+				ind = i;
 			}
 		}
-		else
-			System.out.println("Hai inserito un numero negativo");
-		
+		return ind;
 	}
-	
+
 }
--- src/Main.java
+++ src/Main.java
@@ -7,25 +7,29 @@ public class Main {
 		Scanner input=new Scanner(System.in);
 		int numI;
 		double numV;
-		String str, passw;
+		String str, password;
 		boolean stopProg=false,stopIns=false, erogata=false;
 		
 		Distributore dis=new Distributore();
 		
 		while(!stopProg) {
 			System.out.println("--MENU--");
-			System.out.println("1 per inserire monetine");
+			System.out.println("1 Per inserire monetine");
 			if(dis.getTotMonetine()>0) {
 				System.out.println("2 per scegliere bevanda");
 			}
 			System.out.println("3 per caricare la macchinetta");
+			if(dis.getDenaroTotale()>0) {
+				System.out.println("4 per conoscere incasso totale macchinetta");
+			}
+			
 			str=input.next();
+			System.out.print("\n");
 			
 			switch (str) {
 			case "1":
 				while(!stopIns) {
-					System.out.println("Inserisci monetina o premi 0");
-					System.out.println("per terminare inserimento");
+					System.out.println("--[Inserisci monetina e premi 0 per terminare inserimento]--");
 					numV=input.nextDouble();
 					dis.contaDenaro(numV);
 					if(numV==0) {
@@ -40,18 +44,19 @@ public class Main {
 				dis.erogaBibita(numI);
 				break;
 			case "3":
-				System.out.println("Inserire password");
-				passw=input.next();
-				System.out.println("Cosa vuoi aggiungere? ");
-				str=input.next();
-				System.out.println("Inserisci la quantità ");
-				numI=input.nextInt();
-				dis.login(passw, str, numI);
+				System.out.println("--[Inserire password]--");
+				password=input.next();
+				dis.avviaCarico(dis.verificaPassword(password));				
 				break;
-			default : System.out.println("Uscito dallo switch");
+			case "4":
+				numV=dis.getDenaroTotale();
+				System.out.println("Denaro incassato dal distributore "+numV);
+				break;
+			default : System.out.println("!!!!Opzione non disponibile!");
 					if(!erogata)
 						dis.ritornaSoldi();
-	   		  		stopProg=true;
+					System.out.println("");
+//	   		  		stopProg=true;
 			}
 		}
 		
